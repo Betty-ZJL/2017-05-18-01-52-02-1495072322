80674f991da6328b9adc1d7caff0fc2cf415a21c
Betty-ZJL <Betty-ZJL@users.noreply.github.com>
2017-07-17 15:51:01 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -1,11 +1,11 @@
 module.exports =class Person{
 
     constructor(name,age){
-        this.name=name
-        this.age=age
+        this.name=name;
+        this.age=age;
     }
     introduce(){
-        return('My name is ${this.name}. I am ${this.age} years old.')
+        return('My name is ${this.name}. I am ${this.age} years old.');
     }
     
-}
+};
