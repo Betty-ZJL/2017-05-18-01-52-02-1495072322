7a311813536e7f4d1edd07fba00d4d848f75de03
Betty-ZJL <Betty-ZJL@users.noreply.github.com>
2017-07-17 13:16:54 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -7,6 +7,6 @@ module.exports = class Student extends Person{
         this.klass=klass
     }
     introduce(){
-        return('My nam is ${this.name}. I am ${this.age} years old. I am a Student. I am at Class ${this.klass}.')
+        return('My name is ${this.name}. I am ${this.age} years old. I am a Student. I am at Class ${this.klass}.')
     }
 }
