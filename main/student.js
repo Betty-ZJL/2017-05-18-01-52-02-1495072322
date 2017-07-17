960caee4168c3ff9aa9d10811b217848d2ac1e97
Betty-ZJL <Betty-ZJL@users.noreply.github.com>
2017-07-17 15:52:27 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -1,12 +1,14 @@
+
 let Person = require('./person')
 // Write your code here
 module.exports = class Student extends Person{
 
     constructor(name,age,klass){
-        super(name,age)
-        this.klass=klass
+        super(name,age);
+        this.klass=klass;
     }
     introduce(){
-        return('My name is ${this.name}. I am ${this.age} years old. I am a Student. I am at Class ${this.klass}.')
+        return('My name is ${this.name}. I am ${this.age} years old. I am a Student. I am at Class ${this.klass}.');
     }
-}
+    
+};
